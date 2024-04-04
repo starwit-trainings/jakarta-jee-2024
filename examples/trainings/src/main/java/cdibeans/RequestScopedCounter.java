86c0c750bfaffbb541b3fcc0d86062d4e610053d
Anett <anett.huebner@starwit.de>
2024-04-04 11:51:12 +0200

    added post contruct

--- examples/trainings/src/main/java/cdibeans/RequestScopedCounter.java
+++ examples/trainings/src/main/java/cdibeans/RequestScopedCounter.java
@@ -2,12 +2,18 @@ package cdibeans;
 
 import java.io.Serializable;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
 
 @RequestScoped
 public class RequestScopedCounter implements Serializable {
 
 	private int counter = 0;
+
+	@PostConstruct
+	private void init() {
+		counter = 6;
+	}
 
 	public void count(){
 		counter++;
